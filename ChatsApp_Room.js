6fc25cb8a862d49740d25160650b561988e63145
KrishivAnkitAgarwal29 <104824392+balakrishnan29@users.noreply.github.com>
2022-05-05 14:33:57 +0530

    Add files via upload

---
+++ ChatsApp_Room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6TwumwaoTjsosXMB8lWPeZLHnrXYKmNs",
+    authDomain: "chatsapp-e6b39.firebaseapp.com",
+    databaseURL: "https://chatsapp-e6b39-default-rtdb.firebaseio.com",
+    projectId: "chatsapp-e6b39",
+    storageBucket: "chatsapp-e6b39.appspot.com",
+    messagingSenderId: "928471500697",
+    appId: "1:928471500697:web:b874649db632a7459b3508"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
